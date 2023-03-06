9883c3f3a42efd71064474a548fdb119171ce47a
DoopTu <tuhandsome111@gmail.com>
2023-03-06 10:11:35 +0700

    add chai library

--- test/date.test.js
+++ test/date.test.js
@@ -1,18 +1,19 @@
-const assert = require('assert');
-const {isValidDate } = require('./Functional/utils');
+const assert = require('chai').assert;
+const { isValidDate } = require('./Functional/utils');
 
 describe("IS Valid Date", function () {
-    it("should return true for valid date strings", function () {
-      assert.strictEqual(isValidDate("2022-02-28"), true);
-    });
-  
-    it("should return false for invalid date strings", function () {
-      assert.strictEqual(isValidDate("2022-02-31"), false);
-      assert.strictEqual(isValidDate("2022-02-"), false);
-      assert.strictEqual(isValidDate("22-02-28"), false);
-      assert.strictEqual(isValidDate("2022-13-28"), false);
-      assert.strictEqual(isValidDate("2022-00-28"), false);
-      assert.strictEqual(isValidDate("2022-02-00"), false);
-      assert.strictEqual(isValidDate("2022-02-32"), false);
-    });
+  it("should return true for valid date strings", function () {
+    assert.isTrue(isValidDate("2022-02-28"));
+
   });
+
+  it("should return false for invalid date strings", function () {
+    assert.isFalse(isValidDate("2022-02-31"));
+    assert.isFalse(isValidDate("2022-02-"));
+    assert.isFalse(isValidDate("22-02-28"));
+    assert.isFalse(isValidDate("2022-13-28"));
+    assert.isFalse(isValidDate("2022-00-28"));
+    assert.isFalse(isValidDate("2022-02-00"));
+    assert.isFalse(isValidDate("2022-02-32"));
+  });
+})   
